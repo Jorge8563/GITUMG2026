2186468867fd87bfe599e9cae1c6a87c00a1b2fe
umg <umg@JOC-LAB-1C1.labjoc.local>
2026-04-14 18:37:04 -0600

    mi primera version de mi codigo

---
+++ src/github_umg/Github_umg.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package github_umg;
+
+/**
+ *
+ * @author umg
+ */
+public class Github_umg {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola mundo");
+    }
+    
+}
